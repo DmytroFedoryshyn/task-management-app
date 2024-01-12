d2fec19ad5706a0e185fe7c79cc72ab185151a15
Dmytro Fedoryshyn <51031460+DmytroFedoryshyn@users.noreply.github.com>
2024-01-12 13:27:09 +0200

    Revert "Added Auth"

--- TaskManagementSystem/src/main/java/org/taskmanagement/exception/RegistrationException.java
+++
@@ -1,7 +0,0 @@
-package org.taskmanagement.exception;
-
-public class RegistrationException extends RuntimeException {
-    public RegistrationException(String message) {
-        super(message);
-    }
-}
--- TaskManagementSystem/src/main/java/org/taskmanagement/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.taskmanagement.service;
-
-
-import org.taskmanagement.dto.UserRegistrationRequest;
-import org.taskmanagement.dto.UserResponseDto;
-
-public interface UserService {
-    UserResponseDto registerUser(UserRegistrationRequest request);
-}
